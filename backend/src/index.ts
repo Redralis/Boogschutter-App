26e13b1f03a064f20cfeac114346a643ea48ab19
Jaron van Well <jaronvwell@gmail.com>
2022-06-01 10:58:51 +0200

    added types to request to accept jwt request variables

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,16 @@
+import express from "express";
+import { loginRouter } from "./routes/login.routes";
+import { validateJWTToken } from "./controllers/auth.controller";
+const app = express();
+app.use(express.json());
+const port = 3000;
 
-import express from 'express'
-import { loginRouter } from './routes/login.routes'
-import { validateJWTToken } from './controllers/auth.controller'
-const app = express()
-app.use(express.json())
-const port = 3000
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
 
-app.get('/',validateJWTToken,  (req, res) => {
-  res.send('Hello World!')
-})
-
-app.use("/login/", loginRouter)
+app.use("/login/", loginRouter);
 
 app.listen(port, () => {
-  console.log(`Example app listening on http://localhost:${port}`)
-})
+  console.log(`Example app listening on http://localhost:${port}`);
+});
