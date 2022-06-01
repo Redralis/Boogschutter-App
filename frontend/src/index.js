1320ecc1760324a16cf908b105455521086752c6
Lucas de Kleijn <l.dekleijn2@student.avans.nl>
2022-06-01 12:06:12 +0200

    Slightly changed file structure and navigation panel speed.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './styles/index.css';
 import Chats from "./pages/chats"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
