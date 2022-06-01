9dbd9122fe1f16c4675302a62dfef6f85f3e9316
Jaron van Well <jaronvwell@gmail.com>
2022-06-01 13:54:12 +0200

    switch to mysql, edited prisma schema

--- backend/src/controllers/memberlist.controller.ts
+++ backend/src/controllers/memberlist.controller.ts
@@ -3,29 +3,85 @@ const prisma = new PrismaClient();
 require("dotenv").config();
 
 const getMemberList = async (req: any, res: any) => {
- 
-  if (typeof req.isAdmin !== "undefined" && req.isAdmin) {
-    const data = await prisma.user.findMany({
-      select: {
-        email: true,
-        firstName: true,
-        lastName: true,
-        bondNumber: true,
-        phoneNumber: true,
-      },
-    });
-    data.values
-    res.status(200).json({ result: data });
-  }else {
-    const data = await prisma.user.findMany({
-      select: {
-        email: true,
-        firstName: true,
-        lastName: true,
-      },
-    });
-    data.values
-    res.status(200).json({ result: data });
+  const firstName = req.query.firstname;
+  const lastName = req.query.lastname;
+  console.log(firstName)
+
+  if (typeof firstName !== "undefined") {
+    if (typeof req.isAdmin !== "undefined" && req.isAdmin) {
+      const data = await prisma.user.findMany({
+        where: {
+          firstName: firstName,
+        },
+        select: {
+          email: true,
+          firstName: true,
+          lastName: true,
+          bondNumber: true,
+          phoneNumber: true,
+        },
+      });
+      data.values;
+      res.status(200).json({ result: data });
+    } else {
+      const data = await prisma.user.findMany({
+        select: {
+          firstName: true,
+          lastName: true,
+        },
+      });
+      data.values;
+      res.status(200).json({ result: data });
+    }
+  } else if (typeof lastName !== "undefined") {
+    if (typeof req.isAdmin !== "undefined" && req.isAdmin) {
+      const data = await prisma.user.findMany({
+        where: {
+          lastName,
+        },
+        select: {
+          email: true,
+          firstName: true,
+          lastName: true,
+          bondNumber: true,
+          phoneNumber: true,
+        },
+      });
+      data.values;
+      res.status(200).json({ result: data });
+    } else {
+      const data = await prisma.user.findMany({
+        select: {
+          firstName: true,
+          lastName: true,
+        },
+      });
+      data.values;
+      res.status(200).json({ result: data });
+    }
+  } else{
+    if (typeof req.isAdmin !== "undefined" && req.isAdmin) {
+      const data = await prisma.user.findMany({
+        select: {
+          email: true,
+          firstName: true,
+          lastName: true,
+          bondNumber: true,
+          phoneNumber: true,
+        },
+      });
+      data.values;
+      res.status(200).json({ result: data });
+    } else {
+      const data = await prisma.user.findMany({
+        select: {
+          firstName: true,
+          lastName: true,
+        },
+      });
+      data.values;
+      res.status(200).json({ result: data });
+    }
   }
 };
 
--- backend/src/routes/memberlist.routes.ts
+++ backend/src/routes/memberlist.routes.ts
@@ -2,7 +2,8 @@ import express from "express";
 import { getMemberList } from "../controllers/memberlist.controller";
 const router = express.Router();
 import { body } from "express-validator";
+import { validateJWTToken } from "../controllers/auth.controller"
 
-router.get("/", getMemberList);
+router.get("/", validateJWTToken, getMemberList);
 
 export { router as memberListRouter };
