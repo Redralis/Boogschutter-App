3df3ef775ca29e25a57560ccd7ab972ecd3d222c
Matthijs <matthijs07g@gmail.com>
2022-06-01 11:00:07 +0200

    registratie afgerond

--- backend/src/routes/registratie.route.ts
+++ backend/src/routes/registratie.route.ts
@@ -3,7 +3,7 @@ const { regUser } = require("../controllers/registratie.controller");
 const registratieUser = require("..//controllers/registratie.controller");
 const router = express.Router();
 
-router.put("/api/reg", registratieUser.regUser);
+router.put("/Registratie/", registratieUser.regUser);
 
 export { router };
 module.exports = router;
