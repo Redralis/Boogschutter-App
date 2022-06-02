fce030bf825ca6aa8c6229e9ddf8311846cc63f1
Matthijs <matthijs07g@gmail.com>
2022-06-02 14:26:58 +0200

    test

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
-import logo from '../Images/Logo.png'
-import '../styles/Login.css'
-import resetPassword from './ResetPassword'
+import logo from "../Images/Logo.png";
+import "../styles/Login.css";
+import resetPassword from "./ResetPassword";
 
 export function Login() {
   return (
@@ -9,7 +9,13 @@ export function Login() {
       <div className="container ">
         <main className="form-signin">
           <form>
-            <img className="mb-4" src={logo} alt="" width="72" height="57"></img>
+            <img
+              className="mb-4"
+              src={logo}
+              alt=""
+              width="72"
+              height="57"
+            ></img>
             <h1 className="h3 mb-3 fw-normal">Log in bij uw account</h1>
 
             <div className="form-floating">
@@ -31,47 +37,50 @@ export function Login() {
               ></input>
             </div>
 
+            <div class="form-floating">
+              <label for="floatingPassword">Wachtwoord</label>
+              <input
+                type="password"
+                class="form-control"
+                id="floatingPassword"
+                placeholder="Password"
+              ></input>
+            </div>
+
             <Link to="/contacts">
               <div className="loginButton">
-                <button className="w-100 btn btn-lg " type="submit">
+                <button class="w-100 btn btn-lg " type="submit">
                   Log in
                 </button>
               </div>
-              <div class="form-floating">
-                <label for="floatingPassword">Wachtwoord</label>
-                <input type="password" class="form-control" id="floatingPassword" placeholder="Password"></input>
+            </Link>
+
+            <Link to="/ResetPassword">
+              <div className="passwordLink">
+                <label>Wachtwoord vergeten?</label>
               </div>
-              
-              <Link to="/contacts">
-                <div className="loginButton">
-                  <button class="w-100 btn btn-lg " type="submit">Log in</button>
-                </div>
-              </Link>
+            </Link>
+          </form>
+        </main>
+      </div>
+      <script
+        src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"
+      ></script>
 
-              <Link to="/ResetPassword">
-                <div className="passwordLink">
-                  <label>
-                    Wachtwoord vergeten?
-                  </label>
-                </div>
-              </Link>
+      <script
+        src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"
+        integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ"
+        crossorigin="anonymous"
+      ></script>
 
-            </form>
-          </main>
-        </div>
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-          integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-          crossorigin="anonymous"></script>
-
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"
-          integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ"
-          crossorigin="anonymous"></script>
-
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"
-          integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm"
-          crossorigin="anonymous"></script>
-      </body>
-    </div>
+      <script
+        src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"
+        integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm"
+        crossorigin="anonymous"
+      ></script>
+    </>
   );
 }
 
