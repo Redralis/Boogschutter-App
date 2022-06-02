fcc063088f0fdb2f3f62963ff8dd6bb91517b7f8
hgkarremans <hg.karremans@student.avans.nl>
2022-06-02 13:51:06 +0200

    Changed div to fix css file.

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -5,40 +5,42 @@ import "../styles/Login.css";
 export function Login() {
   return (
     <>
-      <div className="container ">
-        <main className="form-signin">
-          <form>
-            <img className="mb-4" src={logo} alt="" width="72" height="57"></img>
-            <h1 className="h3 mb-3 fw-normal">Log in bij uw account</h1>
+      <div className="loginScreen">
+        <div className="container ">
+          <main className="form-signin">
+            <form>
+              <img className="mb-4" src={logo} alt="" width="72" height="57"></img>
+              <h1 className="h3 mb-3 fw-normal">Log in bij uw account</h1>
 
-            <div className="form-floating">
-              <label for="floatingInput">Email address</label>
-              <input
-                type="email"
-                className="form-control"
-                id="floatingInput"
-                placeholder="naam@voorbeeld.nl"
-              ></input>
-            </div>
-            <div className="form-floating">
-              <label for="floatingPassword">Wachtwoord</label>
-              <input
-                type="password"
-                className="form-control"
-                id="floatingPassword"
-                placeholder="Wachtwoord"
-              ></input>
-            </div>
-
-            <Link to="/contacts">
-              <div className="loginButton">
-                <button className="w-100 btn btn-lg " type="submit">
-                  Log in
-                </button>
+              <div className="form-floating">
+                <label for="floatingInput">Email address</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="floatingInput"
+                  placeholder="naam@voorbeeld.nl"
+                ></input>
               </div>
-            </Link>
-          </form>
-        </main>
+              <div className="form-floating">
+                <label for="floatingPassword">Wachtwoord</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="floatingPassword"
+                  placeholder="Wachtwoord"
+                ></input>
+              </div>
+
+              <Link to="/contacts">
+                <div className="loginButton">
+                  <button className="w-100 btn btn-lg " type="submit">
+                    Log in
+                  </button>
+                </div>
+              </Link>
+            </form>
+          </main>
+        </div>
       </div>
     </>
   );
