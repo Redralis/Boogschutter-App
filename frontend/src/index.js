b44c08e1c24fe75b0f0f1246454bacdd84e5c4b9
Thomas <vanotterloo.thomas@gmail.com>
2022-06-02 17:06:37 +0200

    Added reset password frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Contacts from "./pages/Contacts";
-import Register from './pages/Register';
-import External_links from "./pages/external_links";
+import Contacts from "./pages/Contacts"
+import ResetPassword from "./pages/ResetPassword"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,7 +11,6 @@ root.render(
   <Routes>
     <Route path="/" element={<App />} />
     <Route path="Contacts" element={<Contacts />} />
-    <Route path="Register" element={<Register />} />
-    <Route path="links" element={<External_links />} />
+    <Route path="ResetPassword" element={<ResetPassword/>} />
   </Routes>
 </BrowserRouter>);
