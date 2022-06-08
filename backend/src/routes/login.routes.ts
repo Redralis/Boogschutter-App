4c44f16be12f59316b3b35ecac55bf2b61fb6e05
Jaron van Well <jaronvwell@gmail.com>
2022-06-08 12:36:59 +0200

    users can now participate in an event

--- backend/src/routes/login.routes.ts
+++ backend/src/routes/login.routes.ts
@@ -3,6 +3,12 @@ import { loginErrorHandler, loginUser } from "../controllers/login.controller";
 const router = express.Router();
 import { body } from "express-validator";
 
-router.post("/", body("email").isEmail(),body("password").notEmpty().isString(), loginErrorHandler, loginUser);
+router.post(
+  "/",
+  body("email").isEmail(),
+  body("password").notEmpty().isString(),
+  loginErrorHandler,
+  loginUser
+);
 
 export { router as loginRouter };
