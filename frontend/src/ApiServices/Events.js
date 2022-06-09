83ce172764c382af80ede6eb33f98e0aecbcbeee
Thomas <vanotterloo.thomas@gmail.com>
2022-06-09 20:03:46 +0200

    edited apicall for events

--- frontend/src/ApiServices/Events.js
+++ frontend/src/ApiServices/Events.js
@@ -1,12 +1,16 @@
 const axios = require('axios');
 
-export async function getAllEvents(data) {
-
+export async function getAllEvents() {
+    console.log('get all events')
+    const response = await axios.get(`/event/d`, {
+        data: data
+    })
+    return response.data;
 }
 
 export async function getDailyEvents(date) {
     console.log(date, 'get event from 1 date')
-    const response = await axios.get(`/api/getEvent?Date=${date}`, {
+    const response = await axios.get(`/event/`, {
         data: date
     })
     return response.data;
