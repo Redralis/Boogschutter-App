798d6384ecb2b60c432ec40acc2ea215fdf65ad1
Jaron van Well <jaronvwell@gmail.com>
2022-06-09 11:06:34 +0200

    merged with development and fixed some errors

--- frontend/src/pages/Contacts.js
+++ frontend/src/pages/Contacts.js
@@ -1,23 +1,21 @@
-import TemporaryLoginFirebase from '../components/TemporaryLoginFirebase';
-import {auth} from '../firebase/firebase.js'
-import {useAuthState} from  'react-firebase-hooks/auth'
-import Chats from '../components/Chats';
-import Navbar from '../components/Navbar'
-import '../styles/Contacts.css';
-import { jwtContext } from 'react';
-import AuthChecker from '../components/AuthChecker.jsx';
+import TemporaryLoginFirebase from "../components/TemporaryLoginFirebase";
+import { auth } from "../firebase/firebase.js";
+import { useAuthState } from "react-firebase-hooks/auth";
+import Chats from "../components/Chats";
+import Navbar from "../components/Navbar";
+import "../styles/Contacts.css";
+import { jwtContext } from "react";
+import AuthChecker from "../components/AuthChecker.jsx";
 
 function App() {
   const [user] = useAuthState(auth);
-  
+
   return (
     <>
-    
-      <AuthChecker token={jwtContext} ></AuthChecker>
-    <Navbar />
-    {user ? <Chats/> : <TemporaryLoginFirebase /> }
+      <AuthChecker token={jwtContext}></AuthChecker>
+      <Navbar />
+      {user ? <Chats /> : <TemporaryLoginFirebase />}
     </>
   );
-  
 }
 export default App;
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -6,140 +6,139 @@ import { Link } from "react-router-dom";
 import cookie from "cookie";
 import { jwtContext } from "../components/jwtContext";
 import { Navigate } from "react-router-dom";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import { auth } from "../firebase/firebase.js";
+
+
 
 function Login() {
+
   const jwtContext = createContext("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [credentialError, setCredentialError] = useState(0);
+  const [redirect, setRedirect] = useState(false);
 
-  import "firebase/compat/auth";
-  import "firebase/compat/firestore";
-  import { auth } from "../firebase/firebase.js";
+  function firebaseSignIn() {
+    //    setSelectedChat("")
+    auth.signInWithEmailAndPassword(email, password).catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorCode, errorMessage);
+      console.log(email, password);
+    });
+  }
 
-  function Login() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [credentialError, setCredentialError] = useState(0);
-    const [redirect, setRedirect] = useState(false);
+  function handleEmailChange(event) {
+    setEmail(event.target.value);
+  }
 
-    function firebaseSignIn() {
-      //    setSelectedChat("")
-      auth.signInWithEmailAndPassword(email, password).catch((error) => {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        console.log(errorCode, errorMessage);
-        console.log(email, password);
+  function handlePasswordChange(event) {
+    setPassword(event.target.value);
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    try {
+      const resp = await axios.post("http://localhost:5000/login", {
+        email,
+        password,
       });
-    }
-
-    function handleEmailChange(event) {
-      setEmail(event.target.value);
-    }
-
-    function handlePasswordChange(event) {
-      setPassword(event.target.value);
-    }
-
-    async function handleSubmit(event) {
-      event.preventDefault();
-      try {
-        const resp = await axios.post("http://localhost:5000/login", {
-          email,
-          password,
-        });
-        setCredentialError(200);
-        firebaseSignIn(email, password);
-        console.log(resp.data);
-      } catch (err) {
-        if (err.request.status === 404) {
-          setCredentialError(404);
-        } else if (
-          typeof error.response.data.error !== "undefined" &&
-          error.response.data.error === 400
-        ) {
-          setCredentialError(400);
-        }
+      setCredentialError(200);
+      firebaseSignIn(email, password);
+      console.log(resp.data);
+    } catch (err) {
+      if (err.request.status === 404) {
+        setCredentialError(404);
+      } else if (
+        typeof err.response.data.error !== "undefined" &&
+        err.response.data.error === 400
+      ) {
+        setCredentialError(400);
       }
     }
-
-    return (
-      <div className="loginScreen">
-        <div className="container ">
-          <main className="form-signin">
-            <form onSubmit={handleSubmit.bind(this)}>
-              <img
-                className="mb-4"
-                src={logo}
-                alt=""
-                width="72"
-                height="57"
-              ></img>
-              <h1 className="h3 mb-3 fw-normal">Log in bij uw account</h1>
-              <div className="form-floating">
-                <label htmlFor="floatingInput">Email address</label>
-                <input
-                  type="email"
-                  className="form-control"
-                  id="floatingInput"
-                  placeholder="naam@voorbeeld.nl"
-                  value={email}
-                  onChange={handleEmailChange}
-                ></input>
-              </div>
-              <div className="form-floating">
-                <label htmlFor="floatingPassword">Wachtwoord</label>
-                <input
-                  type="password"
-                  className="form-control"
-                  id="floatingPassword"
-                  placeholder="Wachtwoord"
-                  value={password}
-                  onChange={handlePasswordChange}
-                ></input>
-              </div>
-              <div className="">
-                <input
-                  type="submit"
-                  value="Log in"
-                  className="w-100 btn btn-lg loginButton"
-                />
-              </div>
-
-              <Link to="/resetpassword">
-                <div className="passwordLink">
-                  <label>Wachtwoord vergeten?</label>
-                </div>
-              </Link>
-
-              {credentialError === 404 && (
-                <div
-                  className=" credentialErrorButton alert alert-danger"
-                  role="alert"
-                >
-                  Email/wachtwoord incorrect.
-                </div>
-              )}
-              {credentialError === 400 && (
-                <div
-                  className=" credentialErrorButton alert alert-danger"
-                  role="alert"
-                >
-                  Er is iets mis gegaan
-                </div>
-              )}
-              {credentialError === 200 && (
-                <div
-                  className=" credentialErrorButton alert alert-success"
-                  role="alert"
-                >
-                  Succesvol ingelogd
-                </div>
-              )}
-            </form>
-          </main>
-        </div>
-        {redirect ? <Navigate push to="/contacts" /> : null}
-      </div>
-    );
   }
+
+  return (
+    <div className="loginScreen">
+      <div className="container ">
+        <main className="form-signin">
+          <form onSubmit={handleSubmit.bind(this)}>
+            <img
+              className="mb-4"
+              src={logo}
+              alt=""
+              width="72"
+              height="57"
+            ></img>
+            <h1 className="h3 mb-3 fw-normal">Log in bij uw account</h1>
+            <div className="form-floating">
+              <label htmlFor="floatingInput">Email address</label>
+              <input
+                type="email"
+                className="form-control"
+                id="floatingInput"
+                placeholder="naam@voorbeeld.nl"
+                value={email}
+                onChange={handleEmailChange}
+              ></input>
+            </div>
+            <div className="form-floating">
+              <label htmlFor="floatingPassword">Wachtwoord</label>
+              <input
+                type="password"
+                className="form-control"
+                id="floatingPassword"
+                placeholder="Wachtwoord"
+                value={password}
+                onChange={handlePasswordChange}
+              ></input>
+            </div>
+            <div className="">
+              <input
+                type="submit"
+                value="Log in"
+                className="w-100 btn btn-lg loginButton"
+              />
+            </div>
+
+            <Link to="/resetpassword">
+              <div className="passwordLink">
+                <label>Wachtwoord vergeten?</label>
+              </div>
+            </Link>
+
+            {credentialError === 404 && (
+              <div
+                className=" credentialErrorButton alert alert-danger"
+                role="alert"
+              >
+                Email/wachtwoord incorrect.
+              </div>
+            )}
+            {credentialError === 400 && (
+              <div
+                className=" credentialErrorButton alert alert-danger"
+                role="alert"
+              >
+                Er is iets mis gegaan
+              </div>
+            )}
+            {credentialError === 200 && (
+              <div
+                className=" credentialErrorButton alert alert-success"
+                role="alert"
+              >
+                Succesvol ingelogd
+              </div>
+            )}
+          </form>
+        </main>
+      </div>
+      {redirect ? <Navigate push to="/contacts" /> : null}
+    </div>
+  );
 }
 
 export default Login;
