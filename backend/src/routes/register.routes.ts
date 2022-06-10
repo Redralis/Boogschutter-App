30334963696715906637562c0940cbe09ea84d5f
Lucas de Kleijn <l.dekleijn2@student.avans.nl>
2022-06-10 13:18:24 +0200

    Temporary deletion

--- backend/src/routes/register.routes.ts
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { regUser } = require("../controllers/registratie.controller");
-const registratieUser = require("..//controllers/registratie.controller");
-const router = express.Router();
-
-router.put("/", registratieUser.regUser);
-
-export { router as registerRouter };
--- frontend/src/routes/chats.js
+++
@@ -1,9 +0,0 @@
-import '../styles/app.css';
-
-export default function chats() {
-    return (
-      <main style={{ padding: "1rem 0" }}>
-        <h2>Chats</h2>
-      </main>
-    );
-}
