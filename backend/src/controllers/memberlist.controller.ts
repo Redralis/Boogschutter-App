4f878c32b2895869b5e92874013b8953bbbaaa51
Lucas de Kleijn <l.dekleijn2@student.avans.nl>
2022-06-10 13:21:55 +0200

    Fixed repository

---
+++ backend/src/controllers/memberlist.controller.ts
@@ -0,0 +1,85 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+require("dotenv").config();
+
+const getMemberList = async (req: any, res: any) => {
+  const firstName = req.query.firstname;
+  const lastName = req.query.lastname;
+  console.log(firstName)
+
+  if (typeof firstName !== "undefined") {
+    if (typeof req.isAdmin !== "undefined" && req.isAdmin) {
+      const data = await prisma.user.findMany({
+        where: {
+          firstName: firstName,
+        },
+        select: {
+          email: true,
+          firstName: true,
+          lastName: true,
+          bondNumber: true
+        },
+      });
+      data.values;
+      res.status(200).json({ result: data });
+    } else {
+      const data = await prisma.user.findMany({
+        select: {
+          firstName: true,
+          lastName: true,
+        },
+      });
+      data.values;
+      res.status(200).json({ result: data });
+    }
+  } else if (typeof lastName !== "undefined") {
+    if (typeof req.isAdmin !== "undefined" && req.isAdmin) {
+      const data = await prisma.user.findMany({
+        where: {
+          lastName,
+        },
+        select: {
+          email: true,
+          firstName: true,
+          lastName: true,
+          bondNumber: true
+        },
+      });
+      data.values;
+      res.status(200).json({ result: data });
+    } else {
+      const data = await prisma.user.findMany({
+        select: {
+          firstName: true,
+          lastName: true,
+        },
+      });
+      data.values;
+      res.status(200).json({ result: data });
+    }
+  } else{
+    if (typeof req.isAdmin !== "undefined" && req.isAdmin) {
+      const data = await prisma.user.findMany({
+        select: {
+          email: true,
+          firstName: true,
+          lastName: true,
+          bondNumber: true
+        },
+      });
+      data.values;
+      res.status(200).json({ result: data });
+    } else {
+      const data = await prisma.user.findMany({
+        select: {
+          firstName: true,
+          lastName: true,
+        },
+      });
+      data.values;
+      res.status(200).json({ result: data });
+    }
+  }
+};
+
+export { getMemberList };
---
+++ backend/src/routes/resetPassword.routes.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import { resetPassword , sendEmailForReset } from "../controllers/resetPassword.controller";
+const router = express.Router();
+
+
+
+router.post('/sendMailReset', sendEmailForReset);
+router.put('/resetPassword', resetPassword);
+
+
+export { router };
