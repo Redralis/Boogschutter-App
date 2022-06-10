57a550cea5e9f32603bc76a62904e3e56a6b0c61
Jaron van Well <jaronvwell@gmail.com>
2022-06-10 14:16:07 +0200

    implementing useefect hook

--- frontend/src/pages/Agenda.js
+++ frontend/src/pages/Agenda.js
@@ -1,32 +1,48 @@
-import "../styles/Login.css"
+import "../styles/Login.css";
 import Navbar from "../components/Navbar";
 import AuthChecker from "../components/AuthChecker";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export function Agenda() {
-    return (
-        <>  
-            <AuthChecker></AuthChecker>
-            <Navbar />
-            <div className="loginScreen">
-                <div className="container ">
+  const [events, setEvents] = useState([]);
 
-                    <div className="col-xs-4 row justify-content-between">
-                        <button type="button " className="agenda-buttons float-left">Vorige</button>
-                        <h1 className="display-6 float-center">Datum</h1>
-                        <button type="button " className="agenda-buttons float-right">Volgende</button>
-                    </div>
-                    
-                    <h1 className="float-center display-6"> maandag </h1>
-                    <div className="card">
-                        <div className="card-body">
-                            Masterclass boogschieten
-                        </div>
-                    </div>
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/event")
+      .then(function (response) {
+        console.log(response);
+        setEvents(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
 
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <AuthChecker></AuthChecker>
+      <Navbar />
+      <div className="loginScreen">
+        <div className="container ">
+          <div className="col-xs-4 row justify-content-between">
+            <button type="button " className="agenda-buttons float-left">
+              Vorige
+            </button>
+            <h1 className="display-6 float-center">Datum</h1>
+            <button type="button " className="agenda-buttons float-right">
+              Volgende
+            </button>
+          </div>
+
+          <h1 className="float-center display-6"> maandag </h1>
+          <div className="card">
+            <div className="card-body">Masterclass boogschieten</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Agenda;
