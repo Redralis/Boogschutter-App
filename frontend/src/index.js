8514007cd5c5bf1a0cb6a98a3a5922a6b8d2b147
Jaron van Well <jaronvwell@gmail.com>
2022-06-10 13:02:47 +0200

    idk

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-<<<<<<< HEAD
-import Contacts from "./pages/Contacts"
-import ResetPassword from "./pages/ResetPassword"
-=======
 import Chat from "./pages/Chat";
 import Contacts from "./pages/Contacts"
 import ResetPassword from "./pages/ResetPassword"
@@ -15,17 +11,12 @@ import Register from './pages/Register';
 import UpdatePassword from "./pages/UpdatePassword"
 import Login from "./pages/Login"
 
->>>>>>> fb9e5569e58f5526d820c79b429be4cf4b6c40e7
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 <BrowserRouter>
   <Routes>
     <Route path="/" element={<App />} />
-<<<<<<< HEAD
-    <Route path="Contacts" element={<Contacts />} />
-    <Route path="ResetPassword" element={<ResetPassword/>} />
-=======
     <Route path="Contacten" element={<Contacts />} />
     <Route path="Contacten/Chat" element={<Chat />} />
     <Route path="Documents" element={<Documents />} />
@@ -34,6 +25,5 @@ root.render(
     <Route path="Register" element={<Register/>} />
     <Route path="UpdatePassword" element={<UpdatePassword/>} />
     <Route path="Login" element={<Login/>}/>
->>>>>>> fb9e5569e58f5526d820c79b429be4cf4b6c40e7
   </Routes>
 </BrowserRouter>);
