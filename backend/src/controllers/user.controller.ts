f659d862609e867e371ea5857d70f5397599f4cc
Lucas de Kleijn <l.dekleijn2@student.avans.nl>
2022-06-14 11:26:44 +0200

    Added get user by email

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,8 +1,31 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
-import { User } from "@prisma/client";
-import bcrypt from "bcrypt";
-import { EmitFlags } from "typescript";
+
+const getUser = async (req: any, res: any) => {
+  const { email } = req.body;
+  try {
+    var data = await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+    if (data == null) {
+      res.status(500).json({
+        status: 500,
+        result: "No data found",
+      });
+    }
+    res.status(200).json({
+      status: 200,
+      result: data,
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 404,
+      result: "Failed",
+    });
+  }
+};
 
 const editUser = async (req: any, res: any, next: any) => {
   const { email, isTrainer, isAdmin, isMatchLeader } = req.body;
@@ -64,4 +87,4 @@ const editUser = async (req: any, res: any, next: any) => {
   }
 };
 
-export { editUser }
+export { getUser, editUser }
