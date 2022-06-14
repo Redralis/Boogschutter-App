36efe264fc59562781e4943a9ebda62f59be9e49
Lucas de Kleijn <l.dekleijn2@student.avans.nl>
2022-06-14 10:52:50 +0200

    Added functionality for editing users to be trainers, admins and/or match leaders.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,12 +5,13 @@ import { memberListRouter } from "./routes/memberlist.routes";
 import { eventParticipateRouter } from "./routes/eventParticipate.routes";
 import { router } from "./routes/resetPassword.routes";
 import { eventRouter } from "./routes/event.routes";
+import { userRouter } from "./routes/user.routes";
 import cors from "cors";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-const port = 3060 || process.env.PORT;
+const port = 5000 || process.env.PORT;
 
 
 app.use("/api", router);
@@ -24,6 +25,7 @@ app.use("/register/", registerRouter);
 app.use("/members/", memberListRouter);
 app.use("/participate/", eventParticipateRouter);
 app.use("/event/", eventRouter);
+app.use("/user/", userRouter);
 
 app.all("*", (req, res) => {
   res.status(401).json({
