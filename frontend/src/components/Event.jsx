801b660b8d124aeac3c15d3b7ecdd3ecd02858e7
Jaron van Well <jaronvwell@gmail.com>
2022-06-16 13:36:51 +0200

    events can be retrieved but ignore date filtering

--- frontend/src/components/Event.jsx
+++ frontend/src/components/Event.jsx
@@ -3,14 +3,13 @@ import EventPopup from "../components/EventPopup";
 
 function Event(props) {
   const event = props.event;
-  const dateObject = new Date(event.datePicker);
+  const dateObject = new Date(event.datePicker );
   const convertedDate = dateObject.toLocaleString("nl-nl", {
     dateStyle: "medium",
     timeStyle: "short",
     hour12: false,
   });
 
-  const showPopUpComponent = () => {};
 
   return (
     <>
@@ -20,7 +19,6 @@ function Event(props) {
         date={convertedDate}
       ></EventPopup>
       <button
-        onClick={showPopUpComponent}
         data-toggle="modal"
         data-target={"#" + event.eventId}
       >
