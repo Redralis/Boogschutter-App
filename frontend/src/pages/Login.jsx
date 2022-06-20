8e7b89fb209694852a6fd174451294b499ab5e6d
hgkarremans <hg.karremans@student.avans.nl>
2022-06-20 11:39:10 +0200

    AVG regeling added

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -7,6 +7,9 @@ import { useNavigate } from "react-router-dom";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
 import { auth } from "../firebase/firebase.js";
+import pdf from '../pdf/privacy_statement.pdf'
+
+
 
 function Login() {
   const jwtContext = createContext("");
@@ -24,7 +27,9 @@ function Login() {
     });
   }
 
+
   let navigate = useNavigate();
+
 
   function handleEmailChange(event) {
     setEmail(event.target.value);
@@ -41,6 +46,7 @@ function Login() {
         email,
         password,
       });
+      
       setCredentialError(200);
       firebaseSignIn(email, password);
       localStorage.setItem("token", resp.data.token);
@@ -93,12 +99,18 @@ function Login() {
                 onChange={handlePasswordChange}
               ></input>
             </div>
+            <div className="form-check " style={{ padding: 0 }}>
+              <input type="checkbox" required  />
+              <label className="form-check-label" htmlFor="flexCheckChecked">
+                Ik accepteer de <a href={pdf} edownload="HBSV Alliance d'Amitié privacy verklaring">privacyverklaring</a>
+              </label>
+            </div>
             <div className="">
               <input
                 type="submit"
                 value="Log in"
                 className="w-100 btn btn-lg agenda-buttons"
-                onClick={localStorage.setItem('mail', email)}
+
               />
             </div>
 
