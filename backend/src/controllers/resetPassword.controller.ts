9fae0d74871c11f9164ed37067d66d79c7dc1110
Jaron van Well <jaronvwell@gmail.com>
2022-06-24 15:37:56 +0200

    new resetpassword controller code

--- backend/src/controllers/resetPassword.controller.ts
+++ backend/src/controllers/resetPassword.controller.ts
@@ -5,31 +5,26 @@ import bcrypt, { hash } from "bcrypt";
 const saltRounds = 10;
 
 const resetPassword = async (req: any, res: any) => {
-  
-  
   const { token, email, password } = req.body.data;
-  
 
   if (password !== undefined && email !== undefined && token !== undefined) {
-    
     const user = await prisma.forgotPassword.findMany({
       where: {
         ForgotPasswordUserEmail: email,
-        token: token
+        token: token,
       },
-    })
-    
+    });
 
     if (user.length === 0) {
-      res.status(200).json({
+      return res.status(200).json({
         status: 400,
         password: "Can not find this email in the database.",
-        result: user
+        result: user,
       });
     } else {
       bcrypt.hash(password, saltRounds, async function (err, hash) {
         if (err) {
-          res.status(400).json({
+          return res.status(400).json({
             err,
           });
         }
@@ -42,9 +37,7 @@ const resetPassword = async (req: any, res: any) => {
           },
         });
 
-
-
-        res.status(200).json({
+        return res.status(200).json({
           status: 200,
           result: user,
         });
@@ -54,79 +47,72 @@ const resetPassword = async (req: any, res: any) => {
 };
 
 const sendEmailForReset = async (req: any, res: any) => {
-  
-  
-  let mail = req.body.data;
-  let token: any;
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: "boogschuttervereniging@gmail.com",
-      pass: process.env.GMAIL_PASS,
-    },
-  });
-  // qfwynoehqpcpllug
+  try {
+    let mail = req.body.data;
+    let token: any;
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "boogschuttervereniging@gmail.com",
+        pass: process.env.GMAIL_PASS,
+      },
+    });
 
-  //Generate a random token thats used to reset password
-  require("crypto").randomBytes(
-    3,
-    async function (err: any, buffer: { toString: (arg0: string) => any }) {
-      token = buffer.toString("hex");
-      
+    //Generate a random token thats used to reset password
+    require("crypto").randomBytes(
+      3,
+      async function (err: any, buffer: { toString: (arg0: string) => any }) {
+        token = buffer.toString("hex");
 
-      if (req.body.data !== undefined) {
-        const mailOptions = {
-          from: "boogschuttervereniging@gmail.com",
-          to: req.body.data,
-          subject: "verander je wachtwoord!",
-          text:
-            "kopieer deze code en gebruik deze bij het updaten van je wachtwoord " +
-            "code: " +
-            token,
-        };
+        if (req.body.data !== undefined) {
+          const mailOptions = {
+            from: "boogschuttervereniging@gmail.com",
+            to: req.body.data,
+            subject: "verander je wachtwoord!",
+            text:
+              "kopieer deze code en gebruik deze bij het updaten van je wachtwoord " +
+              "code: " +
+              token,
+          };
 
-        const checkMail = await prisma.user.findMany({
-          where: {
-            email: mail,
-          },
-        });
-
-        if (checkMail.length !== 0) {
-          const addTokenToUser = await prisma.forgotPassword.create({
-            data: {
-              ForgotPasswordUserEmail: req.body.data,
-              token: token,
+          const checkMail = await prisma.user.findMany({
+            where: {
+              email: mail,
             },
           });
-          
-          res.status(200).json({
-            response: addTokenToUser,
-          });
 
-          transporter.sendMail(
-            mailOptions,
-            function (error: any, info: { response: string }) {
-              if (error) {
-                
-                res.status(400).json({ status: 400, response: error });
-              } else {
-                
-                res.status(200).json({
-                  status: 200,
-                  response: info.response,
-                });
+          if (checkMail.length !== 0) {
+            const addTokenToUser = await prisma.forgotPassword.create({
+              data: {
+                ForgotPasswordUserEmail: req.body.data,
+                token: token,
+              },
+            });
+
+            transporter.sendMail(
+              mailOptions,
+              function (error: any, info: { response: string }) {
+                if (error) {
+                  return res.status(400).json({ status: 400, response: error });
+                } else {
+                  return res.status(200).json({
+                    status: 200,
+                    response: "email sent",
+                  });
+                }
               }
-            }
-          );
-        } else {
-          res.status(400).json({
-            response: "DID NOT FIND EMAIL",
-          });
-          
+            );
+          } else {
+            return res.status(400).json({
+              response: "DID NOT FIND EMAIL",
+            });
+          }
         }
       }
-    }
-  );
+    );
+  } catch (err) {
+    throw err;
+  }
 };
 
 export { resetPassword, sendEmailForReset };
